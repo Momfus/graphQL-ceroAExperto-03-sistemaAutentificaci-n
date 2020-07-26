6f2bbcdbc8f6f319ae9b3f78fb43ab6f387ec2a6
Momfus <momfus@outlook.com>
2020-07-26 04:26:46 -0300

    añadido registro de usuarios - clase 122

--- src/resolvers/mutation.ts
+++ src/resolvers/mutation.ts
@@ -1,8 +1,28 @@
 import { IResolvers } from "graphql-tools";
+import { Datetime } from "../lib/datetime";
 
 const mutation : IResolvers = {
     Mutation : {
+        async register( _: void, { user }, { db }): Promise<any> {
 
+            const lastUser = await db.collection('users').find().limit(1).sort({ registerDate: -1 }).toArray(); // Buscar el último usuario conectado (con el último elmento introducido, se coloca -1 para hacer descendente)
+                   
+            // De no haber más usuario, se coloca el primer número, de haber habido más, se aumenta en uno su ID
+            if( lastUser.length === 0 ) {
+                user.id = 1;
+            } else {
+                user.id = lastUser[0].id + 1;
+            }
+            
+            // Fecha de registro
+            user.registerDate = new Datetime().getCurrentDateTime();
+            
+            // Agregar el nuevo elemento
+            await db.collection('users').insertOne(user);
+
+            return user;
+
+        }
     }
 }
 
