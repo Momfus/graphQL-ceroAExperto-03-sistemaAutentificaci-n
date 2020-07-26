6f70f1c93aecc6aad043411c03c7855818a3a80e
Momfus <momfus@outlook.com>
2020-07-26 04:38:18 -0300

    añadio una query (consulta) simple de todos los usuarios - clase 124

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -2,6 +2,11 @@ import { IResolvers } from "graphql-tools";
 
 const query : IResolvers = {
     Query : {
+        async users( _: void, __: any, { db } ): Promise<any> { // Los primeros dos no vamos a usar pero se necesita añadir, por lo que se usan los guion bajo (simples y doble)
+    
+            return await db.collection('users').find().toArray();
+
+        }
 
     }
 }
